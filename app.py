36e9fc60ba1a61c4f2d7abbbfddc73fab77172d5
Owoeye Temitope <owoeyetemitope148@gmail.com>
2022-02-12 13:06:41 +0100

    rewrote the for loops and added writer.save()

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 
+
 def main():
-    src = create_engine('mysql+pymysql://root:password123@localhost/batest')
-    dest = 'postgresql://postgres:samuel1997...@localhost:5432/Training'
-    dest_conn = psycopg2.connect('postgresql://postgres:password123@localhost:5432/Training')
-    table_names = ["batches", "Insufficients", "PrimeAirtimes", "Primeauths", "primedata", "SequelizeMeta", "Transactions", "Users"]
-    writer = pd.ExcelWriter(r'C:\Users\OWOEYE TEMITOPE\Downloads\Online_Retail.xlsx\table_extracts.xlsx')
-    dest_cursor = dest_conn.cursor()
+    src = create_engine('mysql+pymysql://root:password@localhost/database_name')  # connection to the source database
+    dest = 'postgresql://postgres:password@localhost:5432/database_name'   #Connection string from the destination database
+    dest_conn = psycopg2.connect('postgresql://postgres:password@localhost:5432/database_name')  #Connection to the destination database
+    table_names = ["batches", "insufficients", "primeairtimes", "primeauths", "primedata", "sequelizemeta", "transactions", "users"]
+    writer = pd.ExcelWriter(r'C:\Users\OWOEYE TEMITOPE\Downloads\Online_Retail.xlsx\table_extracts.xlsx')  #This writes the data into an excel workbook
+    dest_cursor = dest_conn.cursor()  #cursor for the connection to destination database.
 
     for table_name in table_names:
         query = f'select * from {table_name}'
@@ -23,15 +24,21 @@ def main():
                 camelcase = word_split[0] + ''.join(word.title() for word in word_split[1:])  # This is where the camelCasing is being achieved
                 conv_col_names.append(camelcase)
             chunk.columns = conv_col_names
+
             chunk.to_sql(table_name, con=dest, if_exists='append', index=False, chunksize=10)
+    for table in table_names:
+        query3 = f'select * from {table}'
+        fin_df = pd.read_sql(query3, con=dest)
 
-            df_from_pg = pd.read_sql(query, con=dest)
+        print(fin_df)
+        try:
+            fin_df.to_excel(writer, sheet_name=table, index=False)
+            writer.save()
+            print('Data Spooled to excel successfully')
+        except:
+            print('Data spooling failed')
+    writer.save()
 
-            try:
-                df_from_pg.to_excel(writer, sheet_name=table_name, index=False)
-                print(f'Data  in table {table_name}  Spooled to excel successfully')
-            except:
-                print('Data spooling failed')
 
-if __name__  == "__main__":
+if __name__ == "__main__":
     main()
